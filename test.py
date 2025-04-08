244fd1afd610d3447364ed3292099f9acd5cd6e5
YersonGames <pirno98k@gmail.com>
2025-04-08 18:20:34 -0400

    reqeq3eq

--- test.py
+++ test.py
@@ -1,1 +1,18 @@
-hola
+#While
+count = 0
+
+while count != 100:
+    count += 1
+    print(count)
+
+#For
+numbers = [10,20,30,40]
+
+blocks = ["Cobblestone","Dirt","Grass","Andesite"]
+
+for Block in blocks:
+    print(Block)
+
+for number in numbers:
+    result = number * number
+    print(f"Hola {number} * {number} = {result}")
