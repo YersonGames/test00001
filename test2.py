1ecd17581018cc7983408354e657c9f4d56d5872
YersonGames <pirno98k@gmail.com>
2025-04-15 18:18:01 -0400

    saa

--- test2.py
+++ test2.py
@@ -1,3 +1,4 @@
+#Functions
 def suma(a,b):
     result = a+b
     print(f"el resultado de la suma es: {result}")
@@ -14,38 +15,72 @@ def divi(a,b):
     result = a/b
     print(f"el resultado de la division es: {result}")
 
-print("1) Suma\n2) Resta\n3) Multiplicacion\n4) Division")
+loop3 = False
 
-select = input("Ingrese una opcion: ")
-a = ""
-b = ""
+print("1) Suma\n2) Resta\n3) Multiplicacion\n4) Division\n5) Salir")
 
-cont = False
+while loop3 == False:
+    select = input("Ingrese una opcion: ")
 
-while cont == False:
-    if select.isdigit() == False:
-        select = input("Ingrese una opcion: ")
-    else:
-        select = int(select)
-        if select == 1 or select == 2 or select == 3 or select == 4:
-            cont = True
+    if select == "5":
+        loop3 = True
+        break
+
+
+    #Variables
+    a = ""
+    b = ""
+    cont = False
+    cont2 = False
+    loop1 = False
+    loop2 = False
+
+    while cont == False:
+        if select.isdigit() == False:
+            select = input("Ingrese una opcion: ")
         else:
-            select = ""
+            select = int(select)
+            if select == 1 or select == 2 or select == 3 or select == 4:
+                cont = True
+            else:
+                select = ""
 
-while a.isdigit() == False:
-    a = input("Ingrese el primer numero: ")
+    #Primer numero
+    while loop1 == False:
+        try:
+            a = input("Ingrese el primer numero: ")
+            a = float(a)
+            loop1 = True
+        except ValueError:
+            loop1 = False
 
-while b.isdigit() == False:
-    b = input("Ingrese el segundo numero: ")
-
-a = int(a)
-b = int(b)
-
-if select == 1:
-    suma(a,b)
-elif select == 2:
-    resta(a,b)
-elif select == 3:
-    mult(a,b)
-elif select == 4:
-    divi(a,b)
+    #Segundo numero
+    while loop2 == False:
+        if select != 4: #Suma, resta, multiplicacion
+            try:
+                b = input("Ingrese el segundo numero: ")
+                b = float(b)
+                loop2 = True
+            except ValueError:
+                loop2 = False
+        else: #Si es division
+            try:
+                b = input("Ingrese el segundo numero: ")
+                b = float(b)
+                if b == 0:
+                    print("No se puede dividir por 0")
+                else:
+                    loop2 = True
+            except ValueError:
+                loop2 = False
+            
+    #Calcular
+    if select == 1:
+        suma(a,b)
+    elif select == 2:
+        resta(a,b)
+    elif select == 3:
+        mult(a,b)
+    elif select == 4:
+        divi(a,b)
+    print()
