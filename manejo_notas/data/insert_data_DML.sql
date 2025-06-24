54842cf84acb4ed48ef2ad0ffb05004514229ab5
trecshot <samuelmaripan12@gmail.com>
2025-06-24 16:24:44 -0400

    aa

--- manejo_notas/data/insert_data_DML.sql
+++ manejo_notas/data/insert_data_DML.sql
@@ -1,7 +1,7 @@
 use gestion_notas;
 
 insert into docentes (rut,nombre,email) values 
-("22198823-k","Yerson Cheuquehuala","pirno98k@gmail.com"),
+("","","pirno98k@gmail.com"),
 ("22183741-5","Juan Muñoz","juanmunoz777k@gmail.com");
 
 insert into asignaturas (codigo,nombre,descripcion) values
