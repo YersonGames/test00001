840b7e6455eedb87deab890df14c95ca32a53557
YersonGames <pirno98k@gnail.com>
2025-09-10 11:43:09 -0300

    a

---
+++ sem2/inventario/descript.io
@@ -0,0 +1,5 @@
+inventario_oop
+    - producto.py #clase Producti encapsulamiento
+    - inventario.py #clase inventario CRUD en memoria + carga CSV
+    - app.py #menu de 3 o 4 opciones (programa principal)
+    - data.csv #persistencia
---
+++ sem2/inventario/producto.py
@@ -0,0 +1,59 @@
+class Producto:
+    def __init__(self,id: str,nombre: str,precio: float,stock: int):
+        self.id = None
+        self.nombre = None
+        self.precio = None
+        self.stock = None
+        
+        self.set_id(id)
+        self.set_nombre(nombre)
+        self.set_precio(precio)
+        self.set_stock(stock)
+        
+    #Getters
+    def get_id(self):
+        return self.id
+    def get_nombre(self):
+        return self.nombre
+    def get_precio(self):
+        return self.precio
+    def get_stock(self):
+        return self.stock
+    
+    #Setters
+    def set_id(self, new_id: str):
+        if len(new_id) != 0 and not new_id.strip():
+            raise ValueError ("El codigo no puede estar vacio")
+        self.id = new_id
+    
+    def set_nombre(self, new_nombre: str):
+        if len(new_nombre) != 0 and not new_nombre.strip():
+            raise ValueError ("El nombre no puede estar vacio")
+        self.nombre = new_nombre
+        
+    def set_precio(self, new_precio: float):
+        try:
+            precio = float(new_precio)
+            if precio <= 0:
+                raise ValueError("El precio debe ser mayor a cero")
+            else:
+                self.precio = precio
+        except ValueError:
+            print("El precio debe ser un numero")
+            
+    def set_stock(self, new_stock: int):
+        try:
+            stock = int(new_stock)
+            if stock <= 0:
+                raise ValueError("El stock debe ser mayor a cero")
+            else:
+                self.stock = stock
+        except :
+            raise ValueError("El stock debe ser un numero")
+            
+    def mostrar_info(self):
+        precio_fmt = f"{self.precio:,.0f}".replace(",",".")
+        return f"[{self.id}] {self.nombre} - ${precio_fmt} {self.stock}"
+    
+p = Producto("11aa","Caja",1000,10)
+print(p.mostrar_info())
