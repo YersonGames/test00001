7d4b040c71bc5b93789b6dac47b4d85208f29add
YersonGames <pirno98k@gmail.com>
2025-10-03 13:07:08 -0300

    a

---
+++ sem2/inventario/crear_producto_sp.py
@@ -0,0 +1,39 @@
+import mysql.connector
+
+def main():
+    conexion = mysql.connector.connect(
+                host="127.0.0.1",  # en Windows es mejor 127.0.0.1 que 'localhost'
+                user="root",
+                # password="tu_clave",
+                database="crud",
+                # Si tu MySQL usa 3307, descomenta la línea siguiente:
+                # port=3307,
+            )
+    cursor = conexion.cursor()
+
+    try:
+        nombre = input("Nombre: ")
+        precio = float(input("Precio: "))
+        stock = int(input("Stock: "))
+
+        #el ultimo parametros es el que corresponde al parametro OUT del db
+        parametros = (nombre,precio,stock,0) #placeholder
+
+        #llamar al procedimiento almacenado
+        resultado = cursor.callproc("sp_producto_crear",parametros)
+
+        #el resultado es una lista con corchetes []
+        id_nuevo = resultado[-1] #en python el -1 muestra el ultimo dato de la lista
+        conexion.commit() #confirmar el grabado de datos
+
+        print("Producto creado")
+        print("Nuevo id:",id_nuevo)
+
+    except mysql.connector.Error as error:
+        raise ValueError("Error: ",error)
+
+    finally:
+        cursor.close()
+        conexion.close()
+
+main()
