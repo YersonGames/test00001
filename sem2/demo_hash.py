e92897ea9f62647db453c91df2d98c0bc65c6b79
YersonGames <49105974+YersonGames@users.noreply.github.com>
2025-10-29 11:35:42 -0300

    Update and rename demo_has.py to demo_hash.py

---
+++ sem2/demo_hash.py
@@ -0,0 +1,21 @@
+import hashlib
+import secrets
+import base64
+
+from pwinput import pwinput
+
+password = pwinput("Clave: ")
+print(password)
+
+#Generar sal aleatoria
+sal = secrets.token_bytes(16)
+sal_64 = base64.b64encode(sal).decode() #para guadar como texto (Base64)
+print(sal_64)
+
+#Derivar el hash (byte) usando el algortimo pkdbf2_hmac
+
+iteraciones = 100_000
+hash_b = haslib.pbkf2_hmac("sha256",password.encode("utf-8"),sal,iteraciones)
+print(hash_b)
+hash_b64 = base64.b64encode(hash_b).decode()
+print(hash_b64)
